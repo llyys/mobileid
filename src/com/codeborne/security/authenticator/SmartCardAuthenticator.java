8432bc25a05d7797a5860c6a35a450f516070b2e
laurts <lyys.lauri@gmail.com>
2014-01-24 00:56:37 +0200

    added smartcard authenticator fallback to old certificate loader

--- src/com/codeborne/security/authenticator/SmartCardAuthenticator.java
+++ src/com/codeborne/security/authenticator/SmartCardAuthenticator.java
@@ -1,6 +1,7 @@
 package com.codeborne.security.authenticator;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
 import sun.security.provider.X509Factory;
 
 import javax.security.cert.CertificateException;
@@ -10,19 +11,30 @@ import javax.servlet.http.HttpServletRequest;
 public class SmartCardAuthenticator {
     public static SmartCardUserInfo getUserInfoFromRequest(HttpServletRequest request) throws CertificateException {
         SmartCardUserInfo user=new SmartCardUserInfo();
-        X509Certificate cert = extractClientCertificate(request);
-        String subjectDN = cert.getSubjectDN().getName();
-        user.personalCode = subjectDN.replaceFirst(".*SERIALNUMBER=(\\d{11}),.*", "$1");
-        user.userName = subjectDN;
-        return user;
-    }
+        Object[] attribute = (Object[])request.getAttribute("javax.servlet.request.X509Certificate");
+        if(attribute==null)
+            return null;
+        if(attribute.length==0)
+            return null;
+        Object cert=attribute[0];
+        String subjectDN = null;
 
-    private static X509Certificate extractClientCertificate(HttpServletRequest request) {
-        X509Certificate[] certs = (X509Certificate[]) request.getAttribute("javax.servlet.request.X509Certificate");
-        if (certs != null && certs.length > 0) {
-            return certs[0];
+        if(java.security.cert.X509Certificate.class.isAssignableFrom(cert.getClass()))
+        {
+            java.security.cert.X509Certificate certificate = (java.security.cert.X509Certificate) cert;
+            subjectDN = certificate.getSubjectDN().getName();
         }
-        return null;
+        if(javax.security.cert.X509Certificate.class.isAssignableFrom(cert.getClass()))
+        {
+            java.security.cert.X509Certificate certificate = (java.security.cert.X509Certificate) cert;
+            subjectDN = certificate.getSubjectDN().getName();
+        }
+        if(StringUtils.isNotBlank(subjectDN))
+        {
+            user.personalCode = subjectDN.replaceFirst(".*SERIALNUMBER=(\\d{11}),.*", "$1");
+            user.userName = subjectDN;
+        }
+        return user;
     }
 
 }
